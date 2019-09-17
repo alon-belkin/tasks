11af57838c468098612a09ae89b5c999d07e071c
alon-belkin <55102605+alon-belkin@users.noreply.github.com>
2019-09-17 17:23:31 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,7 +1,7 @@
-print('enter a number: ')
+print('enter a 3 digit number: ')
 x = input()
 if x.isnumeric() and len(x) == 3:
     print(x[::-1])
 else:
-    print(input('enter a 3 digit number: '))
+    print('input should be a 3 digit number')
 
